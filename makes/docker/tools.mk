1bf4d9f3bfb67740958a3b6842c05dda9c648a40
Kamil Samigullin <kamil@samigullin.info>
2017-05-07 21:38:26 +0300

    update makes

--- makes/docker/tools.mk
+++ makes/docker/tools.mk
@@ -1,6 +1,7 @@
 .PHONY: docker-in-tools
 docker-in-tools:
 	docker run --rm -it \
+	           -e GITHUB_TOKEN='${GITHUB_TOKEN}' \
 	           -v '${GOPATH}/src/${GO_PACKAGE}':'/go/src/${GO_PACKAGE}' \
 	           -w '/go/src/${GO_PACKAGE}' \
 	           kamilsk/go-tools:latest \
@@ -10,6 +11,14 @@ docker-in-tools:
 docker-pull-tools:
 	docker pull kamilsk/go-tools:latest
 
+.PHONY: docker-tool-glide
+docker-tool-glide:
+	docker run --rm \
+	           -v '${GOPATH}/src/${GO_PACKAGE}':'/go/src/${GO_PACKAGE}' \
+	           -w '/go/src/${GO_PACKAGE}' \
+	           kamilsk/go-tools:latest \
+	           glide $(COMMAND) $(strip $(ARGS))
+
 .PHONY: docker-tool-gometalinter
 docker-tool-gometalinter:
 	docker run --rm \
@@ -17,14 +26,13 @@ docker-tool-gometalinter:
 	           -w '/go/src/${GO_PACKAGE}' \
 	           kamilsk/go-tools:latest \
 	           /bin/sh -c '$(PACKAGES) | xargs go test -i && \
-	                       gometalinter --vendor $(strip $(ARGS)) ./...'
+	                       gometalinter $(strip $(ARGS))'
 
-.PHONY: docker-tool-glide
-docker-tool-glide: COMMAND = install
-docker-tool-glide:
+.PHONY: docker-tool-goreleaser
+docker-tool-goreleaser:
 	docker run --rm \
+	           -e GITHUB_TOKEN='${GITHUB_TOKEN}' \
 	           -v '${GOPATH}/src/${GO_PACKAGE}':'/go/src/${GO_PACKAGE}' \
 	           -w '/go/src/${GO_PACKAGE}' \
 	           kamilsk/go-tools:latest \
-	           /bin/sh -c 'glide $(COMMAND) --strip-vendor $(strip $(ARGS)) && \
-	                       rm -rf /go/src/.glide'
+	           goreleaser $(strip $(ARGS))
