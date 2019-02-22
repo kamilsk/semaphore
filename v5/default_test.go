f50c25137848698b1b666ad70493ad557e89ab1d
Kamil Samigullin <kamil@samigullin.info>
2019-02-22 15:28:54 +0300

    try fix build

---
+++ v5/default_test.go
@@ -0,0 +1,56 @@
+package semaphore_test
+
+import (
+	"runtime"
+	"testing"
+	"time"
+
+	. "github.com/kamilsk/semaphore/v5"
+)
+
+func TestAcquire(t *testing.T) {
+	rs := make([]ReleaseFunc, 0, Capacity())
+	do := func() {
+		for _, r := range rs {
+			r()
+		}
+	}
+	for i := 0; i < Capacity(); i++ {
+		r, _ := Acquire(nil)
+		rs = append(rs, r)
+	}
+	expected := "operation timeout"
+	if _, err := Acquire(WithTimeout(10 * time.Millisecond)); err.Error() != expected {
+		t.Errorf("an unexpected error. expected: %s; obtained: %v", expected, err)
+	}
+	do()
+	if r, err := Acquire(WithTimeout(10 * time.Millisecond)); err != nil {
+		t.Error("an unexpected error", err)
+	} else {
+		r()
+	}
+}
+
+func TestCapacity(t *testing.T) {
+	if obtained, expected := Capacity(), runtime.GOMAXPROCS(0); obtained != expected {
+		t.Errorf("an unexpected capacity. expected: %d; obtained: %d", expected, obtained)
+	}
+}
+
+func TestOccupied(t *testing.T) {
+	if obtained, expected := Occupied(), 0; obtained != expected {
+		t.Errorf("unexpected occupied places. expected: %d; obtained: %d", expected, obtained)
+	}
+}
+
+func TestRelease(t *testing.T) {
+	if err, expected := Release(), "semaphore is empty"; err.Error() != expected {
+		t.Errorf("an unexpected error. expected: %s; obtained: %v", expected, err)
+	}
+}
+
+func TestSignal(t *testing.T) {
+	if release, ok := <-Signal(nil); release == nil || !ok {
+		t.Error("unexpected signal")
+	}
+}
